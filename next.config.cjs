f5d5534dabac2487b73ad3d896e5bcafb59c7a6a
Paulo Jacomelli <paulo.jacomelli2001@gmail.com>
2026-06-18 22:25:52 -0300

    Migrate next.config.js to CommonJS (next.config.cjs) to fix ES module require error

--- next.config.cjs
+++ next.config.cjs
@@ -12,7 +12,7 @@ const nextConfig = {
         formats: ['image/webp', 'image/avif'],
         deviceSizes: [640, 750, 828, 1080, 1200],
         imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-        minimumCacheTTL: 86400,
+        minimumCacheTTL: 8640,
         remotePatterns: [
             {
                 protocol: 'https',
